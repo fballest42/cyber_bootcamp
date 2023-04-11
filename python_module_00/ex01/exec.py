9608823a8d8236bb7b52f163e1fd5a2bb45a274b
Fernando Ballesteros santamaria <fballest@c3r12s4.42madrid.com>
2023-04-11 14:27:47 +0200

    Open day for cybersecurity bootcamp

---
+++ python_module_00/ex01/exec.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.7
+
+import sys
+print(' '.join(sys.argv[1:])[::-1].swapcase())
---
+++ python_module_00/ex02/whois.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.7
+import sys
+
+def not_int(s):
+    i = len(s) - 1
+    while (i >= 1):
+        if not (s[i] >=)
