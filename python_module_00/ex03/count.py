1158accb63245ffebdcd00ce739708929ea08413
Fernando Ballesteros santamaria <fballest@c2r1s4.42madrid.com>
2023-04-13 11:59:38 +0200

    Solved count problem

--- python_module_00/ex03/count.py
+++ python_module_00/ex03/count.py
@@ -5,30 +5,30 @@ import string
 def text_analyzer(*args):
     """This function counts the number of upper characters, lower characters,
         punctuation and spaces in a given text"""
-    if len(args) > 1:
+    if len(args) > 1 or not isinstance(args[0], str):
         print("ERROR")
         return
     elif (len(args) == 0):
         print("What is the text to analyse?")
         print(">> ", end="")
         sys.stdout.flush()
-        str = sys.stdin.readline()[0:-1]
+        st = sys.stdin.readline()[0:-1]
     else:
-        str = args[0]
+        st = args[0]
     upper = 0
     lower = 0
     punct = 0
     space = 0
-    for i in range(len(str)):
-        if str[i].isupper():
+    for i in range(len(st)):
+        if st[i].isupper():
             upper += 1
-        elif str[i].islower():
+        elif st[i].islower():
             lower += 1
-        elif str[i] in (' ', '\t', '\r', '\v', '\f'):
+        elif st[i] in (' ', '\t', '\r', '\v', '\f'):
             space += 1
-        elif str[i] in (string.punctuation):
+        elif st[i] in (string.punctuation):
             punct += 1
-    print("The text contains {} characters:".format(len(str)))
+    print("The text contains {} characters:".format(len(st)))
     print("- {} upper letters".format(upper))
     print("- {} lower letters".format(lower))
     print("- {} punctuation marks".format(punct))
