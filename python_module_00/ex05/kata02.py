22df52b96a708bc28a99e2c42c39ba54deb8fcca
Fernando Ballesteros santamaria <fballest@c2r1s4.42madrid.com>
2023-04-13 10:47:11 +0200

    Katas update

--- python_module_00/ex05/kata02.py
+++ python_module_00/ex05/kata02.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-t = (3, 30, 2019, 9, 25)
+t = (1971, 2, 28, 18, 35)
 
-if (len(t) >= 5):
-	print("{:>02}/{:>02}/{} {:>02}:{:>2}" 
-    	.format(t[0], t[1], t[2], t[3], t[4]))
+if (len(t) == 5):
+	print("{:>02}/{:>02}/{:>04} {:>02}:{:>2}".format(t[2], t[1], t[0], t[3], t[4]))
 else:
     print("Tuple needs at least five parameters.")
