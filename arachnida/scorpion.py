6b315c458e70fa552a556d23eb949e6fb12f763a
Fernando Ballesteros santamaria <fballest@c1r17s3.42madrid.com>
2023-04-19 16:34:09 +0200

    Aracnida solved

--- arachnida/scorpion.py
+++ arachnida/scorpion.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python3
 
-
 import sys
-import os
-import io
-import piexif
-from PIL import Image, ExifTags
+import exiftool
 
 def getExif(image_name):
-    file_name, file_ext = os.path.splitext(image_name)
-    file_ext = file_ext.lstrip('.')
-    img = Image.open(image_name)
-    # if file_ext == "png":
-    #     img_exif = {"EXIF": piexif.load(img.info.get('png'))}
-    if file_ext in "jpg":
-        img_exif = img.getexif()
-    # elif file_ext == "jpeg":
-    #     img_exif = piexif.load(img.info)
-    if img_exif is None:
-        print('Sorry, image has no exif data.')
-    else:
-        for key, val in img_exif.items():
-            if key in ExifTags.TAGS:
-                print(f'{ExifTags.TAGS[key]}: {val}')
+   with exiftool.ExifToolHelper() as et:
+    metadata = et.get_metadata(image_name)
+    for dic in metadata:
+        print("\n{:*>80s}".format("*"))
+        print("* METADATOS DEL ARCHIVO {:>>53s} *".format(dic['SourceFile'].upper()))
+        print("{:*>80s}".format("*"))
+        for key in dic:
+            print("{:-<40s}> {}".format(key, dic[key]))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        for ar in sys.argv[1:]:
-            getExif(ar)
+        getExif(sys.argv[1:])
     else:
         print("you must provide at least an image")
--- arachnida/spider.py
+++ arachnida/spider.py
@@ -5,18 +5,27 @@ import os
 import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
+import pathlib
+import sys
 
 def download_image(image_url, save_dir):
+    global _num_img
     response = requests.get(image_url)
     if response.status_code == 200:
         filename = os.path.basename(urlparse(image_url).path)
         with open(os.path.join(save_dir, filename), 'wb') as f:
             f.write(response.content)
+            _num_img += 1
 
-def spider(url, recursive, level, save_dir, extensions, visited, dominio):
+def spider(url, recursive, level,save_dir, extensions, visited, dominio):
     if level < 0 or url in visited:
         return
+    global _num_img
+    visited.add(url)
     response = requests.get(url)
+    if response.status_code != 200:
+        print("ERROR WEB", url)
+        return
     if response.url != '#' and response.url != '':
         soup = BeautifulSoup(response.text, 'html.parser')
     for img in soup.find_all('img'):
@@ -25,50 +34,32 @@ def spider(url, recursive, level, save_dir, extensions, visited, dominio):
         if parsed.netloc.endswith(dominio) and parsed.scheme not in\
             ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
             if parsed.scheme not in ('http', 'https', 'file'):
-                print("HOLA2")
                 if src and any(src.endswith(ext) for ext in extensions) and requests.get('http://' + src):
                     download_image('http://' + src, save_dir)
-                    visited.add(url)
                 elif src and any(src.endswith(ext) for ext in extensions) and requests.get('https://' + src):
                     download_image('https://' + src, save_dir)
-                    visited.add(url)
                 elif src and any(src.endswith(ext) for ext in extensions) and requests.get('file://' + src):
                     download_image('file://' + src, save_dir)
-                    visited.add(url)
-                else:
-                    visited.add(url)
             elif parsed.scheme in ('http', 'https', 'file') and any(src.endswith(ext) for ext in extensions):
                 download_image(src, save_dir)
-                visited.add(url)
-            else:
-                visited.add(url)
-        else:
-            visited.add(url)
     for img in soup.find_all('image'):
         src = img.get('href')
         parsed = urlparse(src)
         if parsed.netloc.endswith(dominio) and parsed.scheme not in\
             ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
             if parsed.scheme not in ('http', 'https', 'file'):
-                print("HOLA2")
+                print("HOLA3")
                 if src and any(src.endswith(ext) for ext in extensions) and requests.get('http://' + src):
                     download_image('http://' + src, save_dir)
-                    visited.add(url)
                 elif src and any(src.endswith(ext) for ext in extensions) and requests.get('https://' + src):
                     download_image('https://' + src, save_dir)
-                    visited.add(url)
                 elif src and any(src.endswith(ext) for ext in extensions) and requests.get('file://' + src):
                     download_image('file://' + src, save_dir)
-                    visited.add(url)
-                else:
-                    visited.add(url)
             elif parsed.scheme in ('http', 'https', 'file') and any(src.endswith(ext) for ext in extensions):
-                download_image(src, save_dir)
-                visited.add(url)
-            else:
-                visited.add(url)
-        else:
-            visited.add(url)    
+                num_img = download_image(src, save_dir)
+    sys.stdout.write('\r')
+    sys.stdout.write("Level: [{}] --- Images Located: [{}]\r".format(level, _num_img))
+    sys.stdout.flush()
     if recursive:
         for link in soup.find_all('a', href=True):
             href = link.get('href')
@@ -78,14 +69,13 @@ def spider(url, recursive, level, save_dir, extensions, visited, dominio):
                 href = urljoin(url, href)
             parsed = urlparse(href)
             if not parsed.netloc.endswith(dominio) or parsed.scheme in ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
+                visited.add(urljoin(response.request.url, href))
                 continue
-            print(parsed)
-            if not parsed.scheme or parsed.scheme == '':
                 # href = '{}://{}'.format(parsed.scheme or 'https', parsed.netloc) + parsed.path
-                href = response.request.url + href
-            if href not in visited: # and any(href.endswith(ext) for ext in extensions):
-                spider(href, recursive, level - 1, save_dir, extensions, visited, dominio)           
-    print(visited)
+            href = urljoin(response.request.url, href)
+            if href not in visited and parsed.netloc.endswith(dominio): # and any(href.endswith(ext) for ext in extensions):
+                spider(href, recursive, level - 1, save_dir, extensions, visited, dominio)      
+    # print(visited)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Descarga imágenes de un sitio web')
@@ -100,4 +90,20 @@ if __name__ == '__main__':
     extensions = ('jpg', 'jpeg', 'png', 'gif', 'bmp')
     visited = set()
     dominio = str(urlparse(args.url).netloc).lstrip("www.")
+    global _num_img
+    _num_img = 0
+    lev = args.level
     spider(args.url, args.recursive, args.level, args.path, extensions, visited, dominio)
+    downloaded = 0
+    lst_down = []
+    for path in pathlib.Path("./data").iterdir():
+        if path.is_file():
+            lst_down.append(path.name)
+            downloaded += 1
+    print()
+    print()
+    print("THE LIST OF IMAGES IS:")
+    for el in lst_down:
+        print(el)
+    print()
+    print("For level [{}], I located [{}], but without duplicates you have downloaded [{}] images".format(lev, _num_img, downloaded))
