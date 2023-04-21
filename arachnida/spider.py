31a645e1130965db312dab16e1e94a2639fd2b09
Fernando Ballesteros santamaria <fballest@c1r17s3.42madrid.com>
2023-04-21 14:19:42 +0200

    Max spider 27 for level 0 42madrid

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -7,31 +7,57 @@ from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import pathlib
 import sys
+import re
 
 def image_selector(orig, type, soup, save_dir, extensions, level, dom):
     for img in soup.find_all(orig):
         src = img.get(type)
         if not src or len(src) == 0:
             continue
-        parsed = urlparse(urljoin(dom, src.lstrip(' ')))
-        src = urljoin(dom, src.lstrip(' '))
-        if parsed.scheme not in\
-            ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
-            if parsed.scheme not in ('http', 'https', 'file'):
-                if src and any(src.endswith(ext) for ext in extensions) and requests.get('http://' + src).status_code == 200:
-                    download_image('http://' + src, save_dir)
-                elif src and any(src.endswith(ext) for ext in extensions) and requests.get('https://' + src).status_code == 200:
-                    download_image('https://' + src, save_dir)
-                elif src and any(src.endswith(ext) for ext in extensions) and requests.get('file://' + src).status_code == 200:
-                    download_image('file://' + src, save_dir)
-            elif parsed.scheme in ('http', 'https', 'file', ' http', ' https', ' file') and any(src.endswith(ext) for ext in extensions):
-                download_image(src, save_dir)
+        if len(src.split(',')) > 1:
+            for i in src.split(' '):
+                if i.endswith(extensions):
+                    download_image(i, save_dir)
+        else:            
+            parsed = urlparse(urljoin(dom, src.lstrip(' ')))
+            src = urljoin(dom, parsed.path.lstrip(' '))
+            if parsed.scheme not in\
+                ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
+                if parsed.scheme not in ('http', 'https', 'file'):
+                    if src and any(src.endswith(ext) for ext in extensions) and requests.get('http://' + src).status_code == 200:
+                        download_image('http://' + src, save_dir)
+                    elif src and any(src.endswith(ext) for ext in extensions) and requests.get('https://' + src).status_code == 200:
+                        download_image('https://' + src, save_dir)
+                    elif src and any(src.endswith(ext) for ext in extensions) and requests.get('file://' + src).status_code == 200:
+                        download_image('file://' + src, save_dir)
+                elif parsed.scheme in ('http', 'https', 'file', ' http', ' https', ' file') and any(src.endswith(ext) for ext in extensions):
+                    download_image(src, save_dir)
+        # if src:
+        #     ssrc = src.split(',')
+        #     for i in ssrc:
+        #         sss = i.split(' ')
+        #         for s in sss:
+        #             if s.endswith(extensions):
+        #                 parsed = urlparse(urljoin(dom, s.lstrip(' ')))
+        #                 src = urljoin(dom, parsed.path.lstrip(' '))
+        #                 if parsed.scheme not in\
+        #                     ('ftp', 'gopher', 'imap', 'mailto', 'mms', 'news', 'nntp', 'prospero', 'rsync', 'rtsp', 'rtspu', 'sftp', 'shttp', 'sip', 'sips', 'snews', 'svn', 'svn+ssh', 'telnet', 'wais', 'ws', 'wss'):
+        #                     if parsed.scheme not in ('http', 'https', 'file'):
+        #                         if src and any(src.endswith(ext) for ext in extensions) and requests.get('http://' + src).status_code == 200:
+        #                             download_image('http://' + src, save_dir)
+        #                         elif src and any(src.endswith(ext) for ext in extensions) and requests.get('https://' + src).status_code == 200:
+        #                             download_image('https://' + src, save_dir)
+        #                         elif src and any(src.endswith(ext) for ext in extensions) and requests.get('file://' + src).status_code == 200:
+        #                             download_image('file://' + src, save_dir)
+        #                     elif parsed.scheme in ('http', 'https', 'file', ' http', ' https', ' file') and any(src.endswith(ext) for ext in extensions):
+        #                         download_image(src, save_dir)
         sys.stdout.write('\r')
         sys.stdout.write("Level: [{}] --- Images Located: [{}]\r".format(level, _num_img))
         sys.stdout.flush()
 
 def download_image(image_url, save_dir):
     global _num_img
+
     response = requests.get(image_url)
     if response.status_code == 200:
         filename = os.path.basename(urlparse(image_url).path)
@@ -44,9 +70,15 @@ def spider(url, recursive, level,save_dir, extensions, visited, dominio):
         return
     global _num_img
     visited.add(url)
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("ERROR WEB", url)
+    try:
+        response = requests.head(url)
+        if not "text/html" in response.headers['content-type']:
+            return
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("ERROR WEB", url)
+            return
+    except:
         return
     if response.url != '#' and response.url != '':
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -60,6 +92,26 @@ def spider(url, recursive, level,save_dir, extensions, visited, dominio):
     image_selector('i', 'src', soup, save_dir, extensions, level, dom)
     image_selector('i', 'href', soup, save_dir, extensions, level, dom)
     image_selector('i', 'srcset', soup, save_dir, extensions, level, dom)
+    for link in soup.find_all('div', style=True):
+        stweb = link['style']
+        ptr = re.search("http.*[)]", stweb)
+        if ptr == None:
+            continue
+        url = stweb[ptr.start():ptr.end()-1]
+        p = urlparse(url)
+        upar = urljoin(dom, p.path)
+        if upar.endswith(extensions):
+            download_image(upar, save_dir)
+    for link in soup.find_all('link', href= True):
+        stweb = link['href']
+        ptr = re.search("http.*", stweb)
+        if ptr == None:
+            continue
+        url = stweb[ptr.start():ptr.end()]
+        p = urlparse(url)
+        upar = urljoin(dom, p.path)
+        if upar.endswith(extensions):
+            download_image(upar, save_dir)
     if recursive:
         for link in soup.find_all('a', href=True):
             href = link.get('href')
@@ -73,7 +125,8 @@ def spider(url, recursive, level,save_dir, extensions, visited, dominio):
                 continue
             href = urljoin(response.request.url, href)
             if href not in visited and parsed.netloc.endswith(dominio):
-                spider(href, recursive, level - 1, save_dir, extensions, visited, dominio)      
+                spider(href, recursive, level - 1, save_dir, extensions, visited, dominio)
+
     # print(visited)
 
 if __name__ == '__main__':
