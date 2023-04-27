22e85ab2136db4602c21e614e12623e8995ea6b6
Fernando Ballesteros santamaria <fballest@c1r17s3.42madrid.com>
2023-04-27 08:58:48 +0200

    ft_onion finished and working properly

--- ft_onion/getfiles.sh
+++ ft_onion/getfiles.sh
@@ -3,7 +3,8 @@
 docker create --name dummy -p 80:80 -p 4242:4242 ft_onion
 docker cp dummy:/etc/tor/torrc ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/torrc
 docker cp dummy:/etc/nginx/sites-available/default ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/nginx.conf
-docker cp dummy:/var/www/html/index.nginx-debian.html ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/index.htmml
+docker cp dummy:/var/www/html/index.nginx-debian.html ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/index.html
 docker cp dummy:/var/lib/tor/hidden_service/hostname ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/hostname
+docker cp dummy:/etc/ssh/sshd_config ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/sshd_conf
 docker rm -f dummy
 echo cat ~/42/otros_cursos/doing/cyber_bootcamp/ft_onion/files/hostname
